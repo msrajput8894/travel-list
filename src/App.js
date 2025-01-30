6be0a03d0966b7512f8c77822693495a47cd3fdc
Mahendra Rajput <akashrajput1434@gmail.com>
2025-01-31 01:52:09 +0530

    making the footer dynamic and removed the scroll bars from list

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -118,10 +118,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `🧳 You have ${numItems} items on your list, and you already packed
+        ${packedItems} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
